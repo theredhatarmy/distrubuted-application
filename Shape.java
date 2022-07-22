e93e0c21c3387df10444df3a47e5cbeb1389bce5
Kevin Kimanthi <46777371+codingDude77@users.noreply.github.com>
2022-07-22 03:44:51 -0400

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,10 @@
+import java.rmi.*;
+import java.util.Vector;
+
+public interface Shape extends Remote {
+
+    int getVersion() throws RemoteException;
+
+    GraphicalObject getAllState() throws RemoteException;
+
+}
